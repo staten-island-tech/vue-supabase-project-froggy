e236f3c562061f03671b01d91728433180e917dc
winnie2346 <winniedong68@gmail.com>
2026-05-28 12:32:10 -0400

    follow n unfollow buttons

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,18 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import profiles from '@/views/profiles.vue'
-import post from '@/views/post.vue'
+import follows from '@/views/follows.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/1',
-      name: 'profiles',
-      component: profiles,
-    },
-    {
-      path: '/2',
-      name: 'post',
-      component: post,
+      name: 'follows',
+      component: follows,
     },
   ],
 })
---
+++ vue-project/src/views/follows.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1>followers</h1>
+    <h2>{{ count }}</h2>
+    <button @click="tofollow">follow</button>
+    <button @click="tounfollow">unfollow</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+function tofollow() {
+  count.value++
+}
+function tounfollow() {
+  count.value--
+}
+
+//swap unfollow and follow upon click
+</script>
+
+<style scoped>
+img {
+  width: 200px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 40vh;
+}
+</style>
