47541a121249f43255972f2b5e5ce2323b4cd22c
winnie2346 <winniedong68@gmail.com>
2026-05-28 12:20:48 -0400

    omg all the work is trash gar-bage

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import profiles from '@/views/profiles.vue'
+import post from '@/views/post.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/1',
+      name: 'profiles',
+      component: profiles,
+    },
+    {
+      path: '/2',
+      name: 'post',
+      component: post,
+    },
+  ],
 })
 
 export default router
