cbde74ae72c54be5ebc12ed4bdebfc0512e359b8
winnie2346 <winniedong68@gmail.com>
2026-06-05 12:22:01 -0400

    qasdfn

--- vue-project/src/views/createaccount.vue
+++ vue-project/src/views/createaccount.vue
@@ -6,14 +6,12 @@
     <input class="rowss" v-model="password" type="password" placeholder="Create password" />
 
     <input class="rowss" v-model="password" type="password" placeholder="Confirm password" />
-    
+
     <button class="rowss">Create Account</button>
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
 .rows {
@@ -26,3 +24,40 @@
   margin: 10px;
 }
 </style>
+
+<!-- <template>
+    <div>
+<form action="submit"></form> @submit.prevent="login(user)">
+    <label for="username">Username</label>
+    <input type="text" v-model=" user.username" />
+     <label for="password">Password</label>
+    <input type="password" v-model=" user.password"/>
+    <button type="submit">Login</button>
+    <h2 v-if="Logged In"></h2>
+   <h2 v-else="Log In"></h2>
+
+    </div>
+</template>
+
+<script setup>
+const loggedIn= ref(false)
+function login(user){
+    console.log(user)
+    loggedIn.value=true
+}
+
+<div v-if="type === 'true'">
+  Logged In
+
+ v-else-if="type === 'false'"
+  Log In
+</div>
+
+
+import { reactive, ref } from 'vue'
+const user= reactive({username: '', password:''})
+
+</script>
+<style lang="scss">  </style>
+
+<style lang="scss" scoped></style> -->
