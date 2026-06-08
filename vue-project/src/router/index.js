9e418ad5a9adc9cb437d7c17476eb3d43375a1d0
londonv4 <londonv4@nycstudents.net>
2026-06-08 12:16:25 -0400

    in

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import createpin from '@/views/CreatePin.vue'
+import UserAccount from '@/views/UserAccount.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
 
 {
-  path: '/CreatePin',
-  name: 'CreatePin',
-  component: createpin,
+  path: '/UserAccount',
+  name: 'UserAccount',
+  component: UserAccount,
+},
+
+{
+  path: '/Cr',
+  name: 'Us',
+  component: CreatePost,
 },
   ],
 })
