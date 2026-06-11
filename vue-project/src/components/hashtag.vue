d7aa3173e042a1f3a7280310064d2c126715145c
winnie2346 <winniedong68@gmail.com>
2026-06-11 11:57:37 -0400

    Update hashtag.vue

--- vue-project/src/components/hashtag.vue
+++ vue-project/src/components/hashtag.vue
@@ -5,4 +5,4 @@
 <script setup></script>
 
 <style scoped></style>
-<!-- when you click on tag it brings you to the posts with the same tags. posthash.filter if caption has specific tag filter through it and load on screen.  -->
+<!--tags are created when text has the hash symbol and suggested tags pop up, when you click on tag it brings you to the posts with the same tags. posthash.filter if caption has specific tag filter through it and load on screen.  -->
