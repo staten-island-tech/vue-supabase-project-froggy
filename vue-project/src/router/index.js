11ad7d4203b10692aa8b99fd82f7e311ef5e4f17
winnie2346 <winniedong68@gmail.com>
2026-06-15 12:18:14 -0400

    qw2qwqwqwqwq

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UserAccount from '@/views/UserAccount.vue'
-
+import CreatePost from '@/views/CreatePost.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,7 +9,11 @@ const router = createRouter({
       name: 'useracc',
       component: UserAccount,
     },
-  
+    {
+      path: '/2',
+      name: 'create',
+      component: CreatePost,
+    },
   ],
 })
 
